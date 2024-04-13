345ff66217058bf69589b2e6ecb31a2685f4e761
Ralph Vargas <ralphvargassorm@gmail.com>
2024-04-13 14:22:58 -0400

    file upload api error handling

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -9,29 +9,35 @@ export const config = {
 };
 
 export const POST = async (req, res) => {
-  const formData = await req.formData();
-  const file = formData.get("file");
-
-  if (!file) {
-    return NextResponse.json({ error: "No files received." }, { status: 400 });
-  }
-
-  const buffer = Buffer.from(await file.arrayBuffer());
-
-  const filename = file.name.replaceAll(" ", "_");
-
   try {
-    // Write the file to the specified directory (public/uploads) with the modified filename
+    const formData = await req.formData();
+    const file = formData.get("file");
+
+    if (!file) {
+      return NextResponse.json(
+        { error: "No files received." },
+        { status: 400 }
+      );
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+
+    const filename = file.name.replaceAll(" ", "_");
+
     await writeFile(
       path.join(process.cwd(), "public/uploads/" + filename),
       buffer
     );
 
-    // Return a JSON response with a success message and a 201 status code
-    return NextResponse.json({ Message: "Success", status: 201 });
+    return NextResponse.json({
+      Message: "File uploaded successfully.",
+      status: 201,
+    });
   } catch (error) {
-    // If an error occurs during file writing, log the error and return a JSON response with a failure message and a 500 status code
-    console.log("Error occurred ", error);
-    return NextResponse.json({ Message: "Failed", status: 500 });
+    console.log("Error occurred during file upload:", error);
+    return NextResponse.json({
+      Message: "Failed to upload file.",
+      status: 500,
+    });
   }
 };
