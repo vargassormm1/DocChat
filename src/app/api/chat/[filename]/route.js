8ceeef60be26f9104f7ce3093c0807057f9615b4
Ralph Vargas <ralphvargassorm@gmail.com>
2024-04-13 14:22:15 -0400

    api error handling

--- src/app/api/chat/[filename]/route.js
+++ src/app/api/chat/[filename]/route.js
@@ -2,7 +2,17 @@ import { NextResponse } from "next/server";
 import { getResponse } from "@/utils/ai";
 
 export const POST = async (request, { params }) => {
-  const data = await request.json();
-  const response = await getResponse(params.filename, data);
-  return NextResponse.json({ data: response });
+  try {
+    const data = await request.json();
+
+    if (!data) {
+      throw new Error("Invalid request data");
+    }
+
+    const response = await getResponse(params.filename, data);
+    return NextResponse.json({ data: response });
+  } catch (error) {
+    console.error("Error processing request:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 };
--- src/utils/api.js
+++ src/utils/api.js
@@ -3,14 +3,21 @@ const createUrl = (path) => {
 };
 
 export const getAIResponse = async (fileName, userQuestion) => {
-  const res = await fetch(
-    new Request(createUrl(`/api/chat/${fileName}`), {
-      method: "POST",
-      body: JSON.stringify(userQuestion),
-    })
-  );
-  if (res.ok) {
-    const data = await res.json();
-    return data.data;
+  try {
+    const res = await fetch(
+      new Request(createUrl(`/api/chat/${fileName}`), {
+        method: "POST",
+        body: JSON.stringify(userQuestion),
+      })
+    );
+    if (res.ok) {
+      const data = await res.json();
+      return data.data;
+    } else {
+      throw new Error("Failed to fetch data");
+    }
+  } catch (error) {
+    console.error("Error fetching AI response:", error);
+    throw new Error("Failed to fetch AI response");
   }
 };
